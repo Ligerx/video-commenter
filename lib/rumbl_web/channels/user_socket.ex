93ac0af5590b487851acf3f7294ad1c3e1f392dc
Alex Wang <ipoopfool@gmail.com>
2020-04-04 14:51:33 -0700

    fix typo

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -17,7 +17,7 @@ defmodule RumblWeb.UserSocket do
   # performing token verification on connect.
   @max_age 2 * 7 * 24 * 60 * 60
   def connect(%{"token" => token}, socket, _connect_info) do
-    case Phoenix.Token.verify(socket, "user socket", token, max_page: @max_age) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
       {:ok, user_id} ->
         {:ok, assign(socket, :user_id, user_id)}
 
